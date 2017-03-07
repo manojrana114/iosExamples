dc716ddb278967eb7f50a5dd89caee9cd4f2bc1d
Mpsr <manojpratap.singhrana114@gmail.com>
2017-03-08 00:51:16 +0530

    Updaed service extension

--- SwiftExamples/UNNotificationDemo/UNNotificationDemo/AppDelegate.swift
+++ SwiftExamples/UNNotificationDemo/UNNotificationDemo/AppDelegate.swift
@@ -13,10 +13,11 @@ import UserNotifications
 //3 Trigger Notification(Push, TimeInterval, Calendar and Location)
 //4 Schedule Notification
 
-
+// NOTIFICATION MANAGEMENT: We can access pending and delivered notification: IS used to rremove or update notification(see viewcontroller for code)
+//
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
     
     var window: UIWindow?
     
@@ -32,7 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             if granted{
                 //Enable feature
                 print("Authorized  : \(granted)");
-                //a) PArt of registration process  for remoter notification
+                //a) Part of registration process  for remoter notification
                 UIApplication.shared.registerForRemoteNotifications();
             }else{
             //Disable notification based feature
@@ -40,8 +41,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }};
         
         // 2) Content Creation
-        let content = UNUser
+        let content = UNMutableNotificationContent()
+        content.title = "Notificatio demo"
+        content.subtitle = "Time based notification"
+        content.body = "My first notification"
+        center.delegate = self
         
+        
+        //3) Create trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //4) Package everything into request
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+        //5) Schedule notification
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil{
+            print("Error : \(error)")
+            }
+        }
         
         /* //b We can set category for the notification. We use to associate custom action with notfication and defines how that notification needs to be handled
          
@@ -81,5 +99,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
+    // MARK:UNUserNotificationDelegate : Notification handling ,Needs to set notification center delagate
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Handling notification when app is in Foreground
+        completionHandler([.alert,.sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(response)
+        completionHandler()
+    }
 }
 
--- SwiftExamples/UNNotificationDemo/UNNotificationDemo/ViewController.swift
+++ SwiftExamples/UNNotificationDemo/UNNotificationDemo/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
-
+import UserNotifications
 class ViewController: UIViewController {
-
+    
+    let nIdentifier = "notification" //Notification identifier
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,94 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendNotificaion(_ sender: UIButton) {
+        
+        // 2) Content Creation
+        let content = UNMutableNotificationContent()
+        content.title = "Notificatio demo"
+        content.subtitle = "Time based notification"
+        content.body = "My first notification"
+        
+        
+        //3) Create trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //4) Package everything into request
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        
+        //5) Schedule notification
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil{
+                print("Error : \(error)")
+            }
+        }
+    }
 
+    @IBAction func updateNotificationContent(_ sender: UIButton) {
+        let updatedContent = UNMutableNotificationContent()
+        updatedContent.title = "Updated Notificatio demo"
+        updatedContent.subtitle = " Updated Time based notification"
+        updatedContent.body = "Updated My first notification"
+        
+        //3) Create trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //4) Package everything into request
+        let request = UNNotificationRequest(identifier: "notification", content: updatedContent, trigger: trigger)
+        
+        //5) Schedule notification
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil{
+                print("Error : \(error)")
+            }
+        }
+    }
+    
+    @IBAction func removeNotification(_ sender: Any) {
+        UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [nIdentifier])
+    }
+    
+    
+    @IBAction func addActionNotification(_ sender: Any) {
+        
+        //Create Action
+        let snoozeAction = UNNotificationAction(identifier: "SNOOZE_ACTION", title: "Snooze", options: UNNotificationActionOptions.init(rawValue: 0))
+        
+        let textInputAction = UNTextInputNotificationAction(identifier: "input", title: "Message", options: .init(rawValue: 1), textInputButtonTitle: "Send", textInputPlaceholder: "name")
+        
+        let stopAction = UNNotificationAction(identifier: "STOP_ACTION", title: "Stop", options:.foreground)
+        
+        //create Category
+        let timerCategory = UNNotificationCategory(identifier: "timerCategory", actions: [textInputAction,snoozeAction,stopAction], intentIdentifiers: [], options: [])
+        
+        //Register category with UNNotifictionCenter
+        UNUserNotificationCenter.current().setNotificationCategories([timerCategory])
+        
+        
+        // 2) Content Creation
+        let content = UNMutableNotificationContent()
+        content.title = "Alarm"
+        content.body = "Wake up !!"
+        content.categoryIdentifier = "timerCategory"
+        
+        //3) Create trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //4) Package everything into request
+        let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
+        
+        //5) Schedule notification
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil{
+                print("Error : \(error)")
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func addTextActionNotification(_ sender: Any) {
+        
+    }
 }
 
