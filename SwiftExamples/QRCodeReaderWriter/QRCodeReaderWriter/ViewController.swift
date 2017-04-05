f619f063671382c24bdffb2d0db42080517593aa
Manoj Pratap Singh Rana <manojpratap.singhrana114@gmail.com>
2017-04-05 18:28:48 +0530

    Initial QR code project

---
+++ SwiftExamples/QRCodeReaderWriter/QRCodeReaderWriter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCodeReaderWriter
+//
+//  Created by Manoj Pratap on 05/04/17.
+//  Copyright © 2017 impetus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
